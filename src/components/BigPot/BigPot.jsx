0d21656513b789065796251a9da36a39d43ed697
J4ud <jdavidflorezm@gmail.com>
2025-04-10 03:42:04 -0500

    pot buttons added

--- src/components/BigPot/BigPot.jsx
+++ src/components/BigPot/BigPot.jsx
@@ -3,9 +3,21 @@ import "./BigPot.css"
 
 const BigPot = () => {
   return (
-    <div className='big-pot'>
+    
+    <div className="bigpot-container">
+      <div className='big-pot'>
       
+      </div>
+      <div className="pot-bton-container">
+        <button id="feed" className="bigpot-bton">
+          <img src="" alt="" />
+        </button>
+        <button id="spray" className="bigpot-bton">
+          <img src="" alt="" />
+        </button>
+      </div>
     </div>
+    
   );
 };
 
