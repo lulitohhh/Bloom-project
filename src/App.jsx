2451df748f3e8d1cfbdcd51ecfe809479197efb1
lulitohhh <luisa.mariaprf@gmail.com>
2025-05-03 18:48:44 -0500

    Actualicé activities dentro del app

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Dashboard from './pages/dashboard/dashboard';
 import React from 'react';
 import GardenAlbum from './pages/GardenAlbum'
-import Actividades from './pages/Actividades';
+import Activities from './pages/Actividades';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="/activities" element={<Actividades />} />
+        <Route path="/activities" element={<Activities />} />
         <Route path="/garden" element={<GardenAlbum />} />
         {/* Puedes agregar más rutas aquí si luego tienes más páginas */}
       </Routes>
