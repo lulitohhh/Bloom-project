58ef390bcc1dc0e5b2b2b7cc6cb432d99c479893
lulitohhh <luisa.mariaprf@gmail.com>
2025-05-04 19:41:11 -0500

    comenté para poder seguir trabajando en otras cosas

--- src/components/Coin/Coin.jsx
+++ src/components/Coin/Coin.jsx
@@ -1,12 +1,18 @@
 import "./Coin.css"
 import Coin from '../../assets/images/Coin.png'
-
+//import { useSelector } from 'react-redux';
 
 const CoinCounter = () => {
+  //const coins = useSelector((state) => state.game.coins);
+
   return (
     <div className='Coin-Bar'>
       
-      <img id="CoinIndicator" src={Coin} alt="" />
+      <img id="CoinIndicator" src={Coin} alt="" /> 
+          <div className="coin-counter">
+             
+          </div>
+      
       
     </div>
   );
