192fe747bacce7034eda591ff1f03efab39325f7
lulitohhh <luisa.mariaprf@gmail.com>
2025-05-04 20:12:13 -0500

    Archivo Storage prueba

--- src/redux/storage.js
+++ src/redux/storage.js
@@ -1,1 +1,9 @@
 //Archivo para guardar y leer de localStorage automáticamente al abrir la app.
+export function saveToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+  
+  export function loadFromLocalStorage(key, defaultValue) {
+    const data = localStorage.getItem(key);
+    return data ? JSON.parse(data) : defaultValue;
+  }
