4890cee80f8ee084249bbf8627942343bd8b69bc
lulitohhh <luisa.mariaprf@gmail.com>
2025-05-11 13:44:37 -0500

    agregué una imagen

--- src/components/SessionSummary/SessionSummary.jsx
+++ src/components/SessionSummary/SessionSummary.jsx
@@ -2,25 +2,31 @@ import { useDispatch, useSelector } from 'react-redux';
 import { addCoins } from '../../redux/coinSlice';
 import { resetSessionCoins } from '../../redux/sessionCoinsSlice';
 import './SessionSummary.css';
+import { useNavigate } from 'react-router-dom';
+import NavBar from '../navBar/navBar';
 
-const SessionSummary = ({ onContinue }) => {
+const SessionSummary = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   const sessionTotal = useSelector((state) => state.sessionCoins.sessionTotal);
 
-  const handleContinue = () => {
+  const handleFinish = () => {
     dispatch(addCoins(sessionTotal));
     dispatch(resetSessionCoins());
-    onContinue(); // Ej: navegar al home o tienda
+    navigate('/'); 
   };
 
   return (
+    <>
+    <NavBar></NavBar>
     <div className="session-summary">
+     <img src="/assets/images/celebration.webp" alt="Coin" className="coin-icon" />
       <h2>¡Has ganado {sessionTotal} monedas en esta sesión!</h2>
-      <img src="/assets/images/Coin.png" alt="Coin" className="coin-icon" />
-      <button className="continue-button" onClick={handleContinue}>
+      <button className="continue-button" onClick={handleFinish}>
         Continuar
       </button>
     </div>
+    </>
   );
 };
 
