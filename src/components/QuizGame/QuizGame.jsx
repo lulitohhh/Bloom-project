3ddd6139ddd9894dc64e302e93f7d48c366ea065
lulitohhh <luisa.mariaprf@gmail.com>
2025-05-11 14:58:53 -0500

    limpieza del codigo

--- src/components/QuizGame/QuizGame.jsx
+++ src/components/QuizGame/QuizGame.jsx
@@ -7,7 +7,6 @@ import Header from '../Header/Header';
 import AnswerOption from '../AnswerOption/AnswerOption';
 import NextButton from '../NextButton/NextButton';
 import NavBar from '../navBar/navBar';
-//import { addCoins } from '../../redux/coinSlice'; 
 import { addSessionCoins } from '../../redux/sessionCoinsSlice';
 
 const QuizGame = ({ id, onSuccess }) => {
@@ -17,13 +16,11 @@ const QuizGame = ({ id, onSuccess }) => {
 
   const coinsGiven = useRef(false);
 
-  // Reiniciar estado del quiz al montar
   useEffect(() => {
     dispatch(resetQuiz());
     coinsGiven.current = false;
   }, [id, dispatch]);
 
-  // Agregar monedas solo una vez cuando la respuesta es correcta
   useEffect(() => {
   if (selection && correct && !coinsGiven.current) {
     console.log('Monedas otorgadas ID:', id);
