e34170a07da4bb0723dae73847ade80f3a8c5519
J4ud <jdavidflorezm@gmail.com>
2025-05-13 18:29:58 -0500

    update avatarData.js

--- src/data/avatarData.js
+++ src/data/avatarData.js
@@ -7,9 +7,9 @@ import Avatar6 from '../../assets/avatars/avatar6.png';
 
 export const avatarOptions = [
   { id: 'avatar1', image: Avatar1, alt: 'Avatar estilo cartoon 1' },
-  { id: 'avatar2', image: Avatar1, alt: 'Avatar estilo cartoon 2' },
-  { id: 'avatar3', image: Avatar1, alt: 'Avatar profesional' },
-  { id: 'avatar4', image: Avatar1, alt: 'Avatar animal' },
-  { id: 'avatar5', image: Avatar1, alt: 'Avatar abstracto' },
-  { id: 'avatar6', image: Avatar1, alt: 'Avatar pixel art' }
+  { id: 'avatar2', image: Avatar2, alt: 'Avatar estilo cartoon 2' },
+  { id: 'avatar3', image: Avatar3, alt: 'Avatar profesional' },
+  { id: 'avatar4', image: Avatar4, alt: 'Avatar animal' },
+  { id: 'avatar5', image: Avatar5, alt: 'Avatar abstracto' },
+  { id: 'avatar6', image: Avatar6, alt: 'Avatar pixel art' }
 ];
