e2afd7ab6fb569901ac556392f7328f6bb48fc35
J4ud <jdavidflorezm@gmail.com>
2025-05-15 17:12:56 -0500

    build(deps): update package-lock.json and activitiesBton.jsx

--- src/components/activitiesBton/activitiesBton.jsx
+++ src/components/activitiesBton/activitiesBton.jsx
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./activitiesBton.css"
-
-
-const ActivitiesBton = () => {
-  const navigate = useNavigate();
-
-  const handleNavigateClick = () => {
-    navigate('/activities'); 
-  };
-
-  return (
-    <>
-       <button className='act-button' onClick={handleNavigateClick}>
-      <h1 id="activitext">Play activities!</h1>
-    </button>
-    </>
-  );
-};
-
-=======
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import "./activitiesBton.css"
@@ -40,5 +17,4 @@ const ActivitiesBton = () => {
   );
 };
 
->>>>>>> 4c7bfd8333749cfd766eee20c721bdf54c2e6768
 export default ActivitiesBton;
